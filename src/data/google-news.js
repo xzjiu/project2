60bcd7eaf7de120c6eccfa8f2d3589d8883bb9ac
ml <mliu78@jh.edu>
2022-12-01 17:20:16 -0500

    added bar chart

---
+++ src/data/google-news.js
@@ -0,0 +1,22 @@
+const data = [
+  { name: "nurse", man: 0.2547229, woman: 0.44135594 },
+  { name: "programmer", man: 0.12616901, woman: 0.089927144 },
+  { name: "violence", man: 0.06061824, woman: 0.14214475 },
+  { name: "peace", man: 0.10325294, woman: 0.11378138 },
+  { name: "domestic", man: 0.09022779, woman: 0.120654374 },
+  { name: "crime", man: 0.16668843, woman: 0.13032138 },
+  { name: "science", man: 0.025733905, woman: 0.04712866 },
+  { name: "math", man: 0.018173736, woman: 0.026722662 },
+  { name: "noisy", man: 0.048578843, woman: 0.03554331 },
+  { name: "teacher", man: 0.25000125, woman: 0.31357846 },
+  { name: "thief", man: 0.51382947, woman: 0.4310914 },
+  { name: "sport", man: 0.12930828, woman: 0.09140664 },
+  { name: "rich", man: 0.16205196, woman: 0.11994398 },
+  { name: "respect", man: 0.05854901, woman: -0.021826327 },
+  { name: "astronaut", man: 0.14364444, woman: 0.230088 },
+  { name: "president", man: 0.028424604, woman: 0.062676705 },
+  { name: "conductor", man: 0.23975001, woman: 0.20593329 },
+  { name: "doctor", man: 0.31448963, woman: 0.37945858 },
+];
+
+export default data;
